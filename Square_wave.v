073f65e6e04412fcf2b49c7835adeb1dda195e5a
wangdec <wangdecheng@sohu.com>
2018-04-07 19:57:31 +0800

    Modify DA wave

--- Square_wave.v
+++ Square_wave.v
@@ -6,10 +6,10 @@ module Square_wave
  input wire Sys_Clock,
  input wire nReset
 );
-parameter Duration = 1000;    //持续多少个周期
+parameter Duration = 10000;    //持续多少个周期
 parameter Disable = 65535;    //间隔多少个周期
-parameter Max_Clock_Half_Div=54;   //主时钟为108M，而该值为54则SPI实际时钟为1M.
-parameter Min_Clock_Half_Div=5400;   //主时钟为108M，而该值为5400则SPI实际时钟为10K.
+parameter Max_Clock_Half_Div=5400;   //主时钟为108M，而该值为54则SPI实际时钟为1M.
+parameter Min_Clock_Half_Div=54;   //主时钟为108M，而该值为5400则SPI实际时钟为10K.
 parameter Max_DA_Data_Bit=6;   //最大的输出位数.
 parameter DA_Clock_Half_Div=3;   //DA的输出时钟，3个为18M.
 
@@ -17,12 +17,12 @@ reg Clock_Divide;
 reg Clock_Eable;
 reg DA_Clock_Temp;
 
-reg [15:0]Divide_Num_clk;
-reg [15:0]Divide_Num_clk_Temp;
-reg [15:0]Duration_Num_clk;
-reg [15:0]Disable_Num_clk;
+reg [31:0]Divide_Num_clk;
+reg [31:0]Divide_Num_clk_Temp;
+reg [31:0]Duration_Num_clk;
+reg [31:0]Disable_Num_clk;
 
-reg [15:0]DA_Num_clk;
+reg [31:0]DA_Num_clk;
 reg [13:0]DA_Data_Temp;
 
 //时钟产生
@@ -36,7 +36,7 @@ reg [13:0]DA_Data_Temp;
       else
        begin
 		  DA_Num_clk=DA_Num_clk+1'h1;
-		  if(DA_Num_clk==DA_Clock_Half_Div)
+		  if(DA_Num_clk>=DA_Clock_Half_Div)
 		  	begin
 			 DA_Clock_Temp=~DA_Clock_Temp;	
 			 DA_Num_clk=0;
@@ -55,8 +55,7 @@ reg [13:0]DA_Data_Temp;
 		  Duration_Num_clk=0;
 		  Disable_Num_clk=0;
 		  Divide_Num_clk_Temp=Min_Clock_Half_Div;
-		  DA_Data=0;
-		  DA_Data_Temp=14'b1;
+		  DA_Data_Temp={(14){1'b1}};
 		 end
 		else if(Clock_Eable)    //输出信号计数
 		 begin
@@ -68,13 +67,14 @@ reg [13:0]DA_Data_Temp;
 			   begin
 				 Clock_Divide=~Clock_Divide;
 				 Divide_Num_clk=0;
-				 Divide_Num_clk_Temp=Divide_Num_clk_Temp+1'h1;
+				 if(Clock_Divide)
+				  Divide_Num_clk_Temp=Divide_Num_clk_Temp+Min_Clock_Half_Div;
 				end		  
 			end 
-		  else if(Divide_Num_clk>Max_Clock_Half_Div)
+		  else if(Divide_Num_clk>Max_Clock_Half_Div || Duration_Num_clk>Duration )
 	      begin
 	       Divide_Num_clk=0;
-		    Clock_Divide=1;
+		    Clock_Divide=0;
 		    Duration_Num_clk=0;
 		    Disable_Num_clk=0;
 		    Divide_Num_clk_Temp=Min_Clock_Half_Div;
@@ -84,18 +84,20 @@ reg [13:0]DA_Data_Temp;
 		 end
 		else if(!Clock_Eable)  //暂停输出信号计数
 		 begin
-		  if(Disable_Num_clk[0])
+		  if(Divide_Num_clk[0])
 		    Disable_Num_clk=Disable_Num_clk+1'h1;
 		  if(Disable_Num_clk>Disable)
 		   begin
-			 Disable_Num_clk=0;
 			 Clock_Eable=1;
+			 Clock_Divide=1;
+			 Divide_Num_clk=0;
 			end
+			Divide_Num_clk=Divide_Num_clk+1'h1;		
 		 end
 	 end
 	 
 	 
 assign DA_Clock=DA_Clock_Temp;
-assign DA_Data=DA_Data_Temp>>Max_DA_Data_Bit;
+assign DA_Data=(Clock_Divide)?(DA_Data_Temp>>Max_DA_Data_Bit):{(14){1'b0}};
 
 endmodule
